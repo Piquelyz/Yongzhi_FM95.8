4119d2155e05e034d6d121a275296bd8093006a5
Yongzhi <lyzpique@gmail.com>
2016-07-14 08:53:24 +0800

    Initial Commit

---
+++ Yongzhi_FM95.8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yongzhi_FM95.8
+//
+//  Created by Yongzhi on 7/14/16.
+//  Copyright © 2016 Yongzhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
